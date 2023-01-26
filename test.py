f77cd1c50469877edcdc272cbdd28e8448ea3f2b
Ghada hamoudi <92649537+ghadaha1@users.noreply.github.com>
2023-01-26 21:18:32 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+def main():
+ note1 = int(input("Enter la preumiere note"))
+ note2 = int(input("Enter la deuxieme note"))
+ note3 = int(input("Enter la troisiemme note"))
+ result = (note1 + note2+ note3)  / 3
+ print("la moyenne est "+str(result))
+
+
+
+ if __name__== '__main__':
+    main()
+  
